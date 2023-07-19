4f59ace9c304220417a38b3845c8537da8d62571
J Lorieau <justin@lorieau.com>
2023-07-19 15:08:46 -0500

    Remove missing functions and variables

--- envalidate/cli/messages.py
+++ envalidate/cli/messages.py
@@ -1,6 +1,8 @@
 """Messages for the command-line interface"""
+import typing as t
 import enum
 import sys
+import os
 
 __all__ = ('p_header', 'p_pass', 'p_fail', 'p_bold', 'p_warn')
 
@@ -21,6 +23,7 @@ class Colors(enum.StrEnum):
 def p_header(msg: str, end: str = '\n'):
     """Print a heading"""
     msg = msg.strip() + ' '
+    term_width = os.get_terminal_size()[0]
 
     sys.stdout.write(f'{Colors.MAGENTA}{msg:=<{term_width - 1}}{Colors.RESET}'
                      f'{end}')
@@ -31,16 +34,9 @@ def p_pass(msg: str, end: str = '\n'):
     sys.stdout.write(f'  {Colors.GREEN}✔ {msg.strip()}{Colors.RESET}{end}')
 
 
-def p_fail(msg: str, end: str = '\n', exit_on_fail: t.Optional = None):
+def p_fail(msg: str, end: str = '\n'):
     """Print a message for a failed test"""
-    if exit_on_fail is None:
-        global exit_on_fail_default
-        exit_on_fail = exit_on_fail_default
-
     sys.stderr.write(f'  {Colors.RED}✖ {msg.strip()}{Colors.RESET}{end}')
-
-    if exit_on_fail:
-        exit(1)
 
 
 def p_bold(msg: str, end: str = '\n'):
