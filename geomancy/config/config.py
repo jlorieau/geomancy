914b1958b7a93ffb1a1146dada019009fd2644d4
J Lorieau <justin@lorieau.com>
2023-07-20 09:20:47 -0500

    Implemented singledton for Config class and nested attribute access

--- geomancy/config/config.py
+++ geomancy/config/config.py
@@ -1,25 +1,59 @@
 """An instance-wide configuration"""
 import typing as t
-
-# Singleton dict
-_config = dict()
+from threading import Lock
 
 
 class Config:
-    """A Config manager to get and set configuration parameters.
+    """A thread-safe Config manager to get and set configuration parameters.
 
     Notes
     -----
     The base class is deliberately modified dynamically with descriptors. This
     is because the Config was designed to be configured on the fly.
     """
+    # The root singleton instance
+    _instance: t.Optional['Config'] = None
+
+    # The thread lock
+    _lock: Lock = Lock()
+
+    def __new__(cls, initial: t.Optional[dict] = None, **kwargs):
+        # Create the singleton instance if it hasn't been created
+        if cls._instance is None:
+            # Lock the thread
+            with cls._lock:
+                # Prevent another thread from creating the instance in the
+                # interim
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+
+        if initial is not None:
+            # If an initial dict was specified--i.e. a sub-dict of the
+            # singleton--then return a Config object with its dict replaced
+            # with that dict
+            obj = super().__new__(cls)
+            obj.__dict__ = initial
+            return obj
+        else:
+            # Otherwise return the singleton
+            return cls._instance
 
     def __init__(self, **kwargs):
-        global _config
-        self.__dict__ = _config
-
+        # Set the specified parameters
         for k, v in kwargs.items():
             setattr(self, k, v)
+
+    def __getattribute__(self, key):
+        try:
+            return super().__getattribute__(key)
+        except AttributeError:
+            # Create a dict by default for a missing attribute
+            # This allows nested attribute access
+            self.__dict__[key] = Config(initial=dict())
+            return super().__getattribute__(key)
+
+    def __setattr__(self, key, value):
+        super().__setattr__(key, value)
 
 
 class Parameter:
@@ -28,19 +62,24 @@ class Parameter:
     __slots__ = ('key', '_config')
 
     key: str
-    _config: dict
+    _config: Config
 
     def __init__(self, key):
-        global _config
         self.key = key
-        self._config = _config
+        self._config = Config()
 
     def __repr__(self):
         value = getattr(self._config, self.key)
         return f"Param({self.key}={value})"
 
     def __get__(self, instance, objtype=None):
-        return self._config.get(self.key)
+        # Convert strings with '.' into nested keys
+        keys = self.key.split('.')
+        value = self._config
+        for key in keys:
+            value = getattr(value, key)
+        return value
 
     def __set__(self, instance, value):
-        self._config[self.key] = value
+        setattr(self._config, self.key, value)
+
--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -3,9 +3,9 @@ from geomancy.config.config import Config, Parameter
 
 
 def test_config_getset():
-    """Test the Config get and set methods"""
+    """Test the Config and Parameter get/set methods"""
 
-    config = Config(value1='value1', value2=3)  # new instance--bad!
+    config = Config(value1='value1', value2=3)
 
     class A:
         attribute1 = Parameter('value1')
@@ -18,7 +18,7 @@ def test_config_getset():
     assert a.attribute1 == 'value1'
     assert a.attribute2 == 3
 
-    # Try modifying the values
+    # Try modifying the values on the config
     config.value1 = 'new value1'
     config.value2 = 4
 
@@ -26,3 +26,26 @@ def test_config_getset():
     assert config.value2 == 4
     assert a.attribute1 == 'new value1'
     assert a.attribute2 == 4
+
+
+def test_config_nested():
+    """Test the Config and Parameter with nested attributes """
+    config = Config()
+
+    # Try modifying the config
+    config.general.value1 = 'value 1'
+    config.general.value2 = 2
+
+    # Try accessing through class properties
+    class A:
+        attribute1 = Parameter('general.value1')
+        attribute2 = Parameter('general.value2')
+    a = A()
+
+    # Try measuring the values directly on the Config singleton
+    assert config.general.value1 == 'value 1'
+    assert config.general.value2 == 2
+
+    # Try measuring the values from class attributes
+    assert A.attribute1 == 'value 1'
+    assert A.attribute2 == 2
