3aa5aba0de9302d3a30fd26527c4f608643a4918
J Lorieau <justin@lorieau.com>
2023-07-21 05:57:02 -0500

    Add metadata for the project description and creating version strings

--- geomancy/__init__.py
+++ geomancy/__init__.py
@@ -1,2 +1,21 @@
 # Current package version
-__version__ = (0, 1, 0, "alpha", 0)
+__version__ = (0, 1, 0, "a")  # Major, minor, patch, stage
+
+# Current description
+__description__ = "Geomancy validates deployment and development environments"
+
+
+def get_version(version=__version__):
+    """Generate a string from the __version__
+
+    Examples
+    --------
+    >>> get_version((0, 1, 0, 'b'))
+    '0.1.0b'
+    >>> get_version((1, 0, 0))
+    '1.0.0'
+    """
+    if isinstance(version[-1], str):
+        return ".".join(map(str, version[:-1])) + version[-1]
+    else:
+        return ".".join(map(str, version))
