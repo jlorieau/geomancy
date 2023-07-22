4b5e91986eff871dbaa3d0711f13f61006524117
J Lorieau <justin@lorieau.com>
2023-07-22 06:28:09 -0500

    Implement sub_env to work with different python types

--- geomancy/checks/utils.py
+++ geomancy/checks/utils.py
@@ -4,7 +4,14 @@ import os
 __all__ = ('sub_env',)
 
 
-def sub_env(string: str):
-    """Substitutes environment variables of the form {VARIABLE_NAME} in the
-    string"""
-    return string.format(**os.environ)
+def sub_env(obj):
+    """Substitutes environment variables of the form {VARIABLE_NAME} in
+    strings"""
+    if isinstance(obj, str):
+        return obj.format(**os.environ)
+    elif hasattr(obj, "__iter__"):
+        o_type = type(obj)
+        items = obj.items() if hasattr(obj, "items") else obj
+        return o_type(sub_env(i) for i in items)
+    else:
+        return obj
---
+++ tests/checks/test_utils.py
@@ -0,0 +1,25 @@
+"""Test check utilities"""
+from pytest import MonkeyPatch
+
+from geomancy.checks.utils import sub_env
+
+
+def test_sub_env():
+    """Test the sub_env environment substitution."""
+
+    with MonkeyPatch.context() as mp:
+        mp.setenv("VAR1", "variable1")
+        mp.setenv("VAR2", "variable2")
+
+        # Test with strings
+        assert sub_env("{VAR1}") == "variable1"
+        assert sub_env("{VAR2}") == "variable2"
+
+        # Test with tuples
+        assert sub_env(("{VAR1}", "{VAR2}")) == ("variable1", "variable2")
+
+        # Test with lists
+        assert sub_env(["{VAR1}", "{VAR2}"]) == ["variable1", "variable2"]
+
+        # Test with dicts and dict-likes
+        assert sub_env({"{VAR1}": "{VAR2}"}) == {"variable1": "variable2"}
