1ec00e0169b76fc7a5327beb9ab810ca27bd8528
J Lorieau <justin@lorieau.com>
2023-07-23 11:26:44 -0500

    Version bump

--- geomancy/__init__.py
+++ geomancy/__init__.py
@@ -4,7 +4,7 @@ from . import checks, cli, config
 __description__ = "Geomancy validates deployment and development environments"
 
 # Project version
-__version__ = (0, 3, 0, "a")  # Major, minor, patch, stage
+__version__ = (0, 4, 0, "a")  # Major, minor, patch, stage
 
 
 def get_version(version=__version__):
