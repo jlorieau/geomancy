144bb9a0b97148add6b5b0131c7709beabf101f9
J Lorieau <justin@lorieau.com>
2023-07-23 14:39:04 -0500

    Add tests for commands which do not have versions

--- tests/checks/test_exec.py
+++ tests/checks/test_exec.py
@@ -16,7 +16,7 @@ def test_check_exec_exists():
 
 
 def test_check_exec_version():
-    """Tests CheckExect checking with version number"""
+    """Tests CheckExec checking with version number"""
     # Should be greater than version 2.0
     check = CheckExec(name="Check Python", value="python>=2.0")
     assert check.check().passed
@@ -24,3 +24,14 @@ def test_check_exec_version():
     # Should be less than version 1000.
     check = CheckExec(name="Check Python", value="python>=1000.0")
     assert not check.check().passed
+
+
+def test_check_exec_missing_version():
+    """Tests CheckExec check with a command that doesn't give a version number"""
+    # Command without a version number
+    check = CheckExec(name="Check ls", value="ls>=2.0")
+    assert not check.check().passed
+
+    # Without a version number, the check passes
+    check = CheckExec(name="Check ls", value="ls")
+    assert check.check().passed
