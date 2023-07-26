f305df5e5ba24fb6a2e1ee1a9f165abaf83c6634
J Lorieau <justin@lorieau.com>
2023-07-26 09:29:35 -0500

    Add an example of a flat checks file without nesting

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 """Test the main CLI entrypoint"""
 import typing as t
 from pathlib import Path
+from itertools import chain
 import os
 
 import pytest
@@ -83,15 +84,13 @@ def test_cli_env(run, flag, test_env_file):
 
 @pytest.mark.parametrize(
     "options",
-    (
-        Path("examples") / "geomancy.toml",
-        Path("examples") / "pyproject.toml",
-        Path("Examples") / "geomancy.yaml",
-    ),
+    chain(*tuple(Path("examples").glob(f"*.{ext}") for ext in ("toml", "yaml"))),
 )
 def test_cli_check(run, options):
-    """Test the default checks"""
+    """Test all the checks files in the examples directory"""
     captured = run(str(options))
 
     # Check, for example, that environment variables were checked
-    assert "Check environment variable" in captured.out
+    assert any(
+        msg in captured.out for msg in ("Check environment variable", "Check path")
+    )
