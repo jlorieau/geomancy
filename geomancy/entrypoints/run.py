59969b29a89e5a581da79b58208893d3ebaa4c64
J Lorieau <justin@lorieau.com>
2023-07-27 20:15:47 -0500

    Implement run subcommand

--- geomancy/entrypoints/run.py
+++ geomancy/entrypoints/run.py
@@ -1,6 +1,7 @@
 """
 The 'run' subcommand
 """
+import subprocess
 import logging
 
 import click
@@ -12,9 +13,16 @@ __all__ = ("run",)
 logger = logging.getLogger(__name__)
 
 
-@click.command
+@click.command(context_settings={"ignore_unknown_options": True})
 @env_options
-def run(**kwargs):
+@click.argument("args", nargs=-1)
+def run(args, **kwargs):
     """Run command within environment"""
+    logger.debug(f"args={args}")
+
     # Hand environment file options
-    env_count = handle_env(**kwargs)
+    env = handle_env(**kwargs)
+
+    # Run the command
+    result = subprocess.run(args, env=env)
+    return result.returncode
