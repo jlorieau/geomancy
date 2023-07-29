6c3e188ef324041bf1fd897be2d626d19e2bcdd6
J Lorieau <justin@lorieau.com>
2023-07-29 11:47:18 -0500

    Shortened status message for mismatched regex

--- geomancy/checks/env.py
+++ geomancy/checks/env.py
@@ -42,7 +42,7 @@ class CheckEnv(CheckBase):
             status = "empty string"
         elif isinstance(self.regex, str) and re.match(self.regex, value) is None:
             # Check the regex, if specified
-            status = "value does not match regex " "'{regex}'".format(regex=self.regex)
+            status = f"does not match regex '{self.regex}'"
         else:
             # All checks passed!
             status = "passed"
