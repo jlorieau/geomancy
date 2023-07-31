eb648dc2d995c1f55e3657de1a2d8aa78d3a46fc
J Lorieau <justin@lorieau.com>
2023-07-31 15:21:13 -0500

    Implement S3 checks for private buckets

--- geomancy/checks/aws/s3.py
+++ geomancy/checks/aws/s3.py
@@ -10,9 +10,17 @@ __all__ = ("CheckAWSS3",)
 
 logger = logging.getLogger(__name__)
 
+# The error message to show when AWS modules can't be imported
+import_error_msg = (
+    "The 'aws' dependency is not installed: {exception}. "
+    "Please reinstall with the '[aws]' or '[all]' extra install "
+    '`python -m pip install "geomancy[aws]"` or '
+    '`python -m pip install "geomancy[all]"`'
+)
+
 
 class CheckAWSS3BucketAccess(CheckBase):
-    """Check AWS S3 buck availability"""
+    """Check AWS S3 bucket availability"""
 
     # The message for checking AWS S3 bucket access
     msg = Parameter(
@@ -21,24 +29,15 @@ class CheckAWSS3BucketAccess(CheckBase):
     )
 
     # The ImportError message to display if aws modules cannot be loaded
-    import_error_msg = (
-        "The 'aws' dependency is not installed: {exception}. "
-        f"Please reinstall with the '[aws]' or '[all]' extra install "
-        f'`python -m pip install "geomancy[aws]"` or '
-        f'`python -m pip install "geomancy[all]"`'
-    )
+    import_error_msg = import_error_msg
 
     def check(self, level: int = 0) -> CheckResult:
         """Check the availability and access to S3 Bucket"""
         msg = self.msg.format(check=self)
 
-        # Get the needed modules
+        # Get the needed modules, bucket name and boto3 client
         boto3, exceptions = self.import_modules("boto3", "botocore.exceptions")
-
-        # Get the bucket name from the check value
         bucket_name = self.value.strip()
-
-        # Get access to the AWS SDK
         s3 = boto3.client("s3")
 
         # Retrieve information on the bucket
@@ -97,8 +96,96 @@ class CheckAWSS3BucketAccess(CheckBase):
             return CheckResult(passed=False, msg=msg, status="unknown reason")
 
 
+class CheckAWSS3BucketPrivate(CheckBase):
+    """Check AWS S3 buck availability"""
+
+    # The message for checking AWS S3 bucket access
+    msg = Parameter(
+        "CHECKAWSS3BUCKETPRIVATE.MSG",
+        default="Check AWS S3 bucket with only private access '{check.value}'...",
+    )
+
+    # The ImportError message to display if aws modules cannot be loaded
+    import_error_msg = import_error_msg
+
+    def check(self, level: int = 0) -> CheckResult:
+        """Check the availability and access to S3 Bucket.
+
+        See: https://stackoverflow.com/a/59002759/9099988
+        """
+        msg = self.msg.format(check=self)
+
+        # Get the needed modules, bucket name and boto3 client
+        boto3, exceptions = self.import_modules("boto3", "botocore.exceptions")
+        bucket_name = self.value.strip()
+        s3 = boto3.client("s3")
+
+        # 1. Check the PublicAccessBlock
+        try:
+            response = s3.get_public_access_block(Bucket=bucket_name)
+        except (exceptions.BotoCoreError, exceptions.ClientError):
+            return CheckResult(
+                passed=False, msg=msg, status="Couldn't access PublicAccessBlock"
+            )
+
+        section = response.get("PublicAccessBlockConfiguration", {})
+        block_public_policy = section.get("BlockPublicPolicy")
+        block_public_acls = section.get("BlockPublicAcls")
+
+        if block_public_policy and block_public_acls:
+            return CheckResult(passed=True, msg=msg, status="passed")
+
+        logger.debug(f"Bucket '{bucket_name}' does not block all public access")
+
+        # 2. Check the Bucket policy to see if it allows public access
+        try:
+            response = s3.get_bucket_policy_status(Bucket=bucket_name)
+        except exceptions.ClientError:
+            # No policy found
+            response = {}
+
+        # See if a policy was set and whether it allows public access
+        section = response.get("PolicyStatus", {})
+        public_policy = section.get("IsPublic", False)
+
+        logger.debug(f"Bucket '{bucket_name}' public policy is: {public_policy}")
+
+        # 3. Check the Bucket ACL to see if it allows public access
+        try:
+            response = s3.get_bucket_acl(Bucket=bucket_name)
+        except exceptions.ClientError:
+            # No policy found
+            response = {}
+
+        # See if ACL grants access
+        section = response.get("Grants", {})
+        grantee_types = [item.get("Grantee", {}).get("Type", "") for item in section]
+        public_acl = any(grantee_type == "Group" for grantee_type in grantee_types)
+
+        logger.debug(f"Bucket '{bucket_name}' ACL to groups is: {public_acl}")
+
+        # Summary
+        # If the policy is false and the acl doesn't have a grantee type of 'group',
+        # then it should be private
+        if not public_policy and not public_acl:
+            return CheckResult(passed=True, msg=msg, status="passed")
+
+        # At this stage, all the checks for private have failed, and it appears
+        # that the s3 bucket is publicly accessible
+        return CheckResult(passed=False, msg=msg, status="publicly accessible")
+
+
 class CheckAWSS3(CheckBase):
-    """Check AWS the availability and permissions of S3 buckets"""
+    """Check AWS the availability and permissions of S3 buckets.
+
+    Notes
+    -----
+    The child checks are meant to verify the accessibility of the bucket as well
+    as optionally test for the best security practices`. Current, these include:
+        - Default private access (CheckAWSS3BucketPrivate)
+
+    See: https://docs.aws.amazon.com/AmazonS3/latest/userguide/security-best-practices.html
+    """
 
     # Whether to check that the bucket is private
     private: bool = True
@@ -113,7 +200,7 @@ class CheckAWSS3(CheckBase):
     )
 
     # Other names for this check
-    aliases = ("checkAWSS3", "checkAwsS3", "CheckAwsS3")
+    aliases = ("checkAWSS3", "checkAwsS3", "CheckAwsS3", "checkS3", "CheckS3")
 
     def __init__(self, **kwargs):
         # Set up keyword arguments
@@ -124,6 +211,14 @@ class CheckAWSS3(CheckBase):
 
         # Replace children with bucket sub-checls
         self.children.clear()
-        self.children += [
+
+        # Bucket accessibility check
+        self.children.append(
             CheckAWSS3BucketAccess(name=f"{self.name}Access", value=self.value)
-        ]
+        )
+
+        # Bucket public access
+        if self.private:
+            self.children.append(
+                CheckAWSS3BucketPrivate(name=f"{self.name}Private", value=self.value)
+            )
--- tests/checks/aws/test_s3.py
+++ tests/checks/aws/test_s3.py
@@ -4,10 +4,12 @@ import pytest
 
 from geomancy.checks.aws.s3 import CheckAWSS3
 
+# Basic accessibility testss
+
 
 @pytest.mark.vcr
 def test_check_aws_s3_missing():
-    """Test CheckAWSS3 check with a missing bucket"""
+    """Test CheckAWSS3/CheckAWSS3BucketAccess check with a missing bucket"""
     bucket_name = "my-missing-bucket-noexist"
 
     # Run the check, which should not pass
@@ -24,7 +26,8 @@ def test_check_aws_s3_missing():
 
 @pytest.mark.vcr
 def test_check_aws_s3_forbidden():
-    """Test CheckAWSS3 check with a bucket that has forbidden access"""
+    """Test CheckAWSS3/CheckAWSS3BucketAccess check with a bucket that has
+    forbidden access"""
     bucket_name = "docker"
 
     # Run the check, which should not pass
@@ -40,7 +43,8 @@ def test_check_aws_s3_forbidden():
 
 
 def test_check_aws_s3_invalid():
-    """Test CheckAWSS3 check with a bucket that has an invalid name"""
+    """Test CheckAWSS3/CheckAWSS3BucketAccess check with a bucket that has an
+    invalid name"""
     bucket_name = "!invalid!"
 
     # Run the check, which should not pass
@@ -55,10 +59,73 @@ def test_check_aws_s3_invalid():
     assert child_results[0].status.startswith("Invalid bucket name")
 
 
+# The following are public/private bucket tests
+@pytest.mark.vcr
+def test_check_aws_s3_without_public_access_block():
+    """Test CheckAWSS3/CheckAWSS3BucketPrivate check using a bucket with
+    Public Access Block turned off."""
+    # The following is a bucket created with "Block Public Access" turned off
+    # but without a policy or ACL specified. Altogether, it should not give
+    # public access
+    bucket_name = "testbucket-nopublicblock"
+
+    # Run the check, which should not pass
+    check = CheckAWSS3(name="Public Bucket", value=bucket_name)
+    result = check.check()
+    assert result.passed
+
+    # Validate the result of the second child check, CheckAWSS3BucketAccess,
+    # which found the bucket name to be invalid
+    child_results = result._child_results
+    assert child_results[1].passed
+
+
+@pytest.mark.vcr
+def test_check_aws_s3_public_policy():
+    """Test CheckAWSS3/CheckAWSS3BucketPrivate check using a bucket with a
+    public Policy."""
+    # The following is a bucket created with "Block Public Access" turned off
+    # and with a policy that allows public access. This bucket should be
+    # publicly accessible and will fail the test
+    bucket_name = "testbucket-public-policy"
+
+    # Run the check, which should not pass
+    check = CheckAWSS3(name="Public Bucket", value=bucket_name)
+    result = check.check()
+    assert not result.passed
+
+    # Validate the result of the second child check, CheckAWSS3BucketAccess,
+    # which found the bucket name to be invalid
+    child_results = result._child_results
+    assert not child_results[1].passed
+    assert child_results[1].status.startswith("publicly accessible")
+
+
+@pytest.mark.vcr
+def test_check_aws_s3_public_acl():
+    """Test CheckAWSS3/CheckAWSS3BucketPrivate check using a bucket with a
+    public ACL."""
+    # The following is a bucket created with "Block Public Access" turned off
+    # and with a policy that allows public access. This bucket should be
+    # publicly accessible and will fail the test
+    bucket_name = "testbucket-alluser-acl"
+
+    # Run the check, which should not pass
+    check = CheckAWSS3(name="Public Bucket", value=bucket_name)
+    result = check.check()
+    assert not result.passed
+
+    # Validate the result of the second child check, CheckAWSS3BucketAccess,
+    # which found the bucket name to be invalid
+    child_results = result._child_results
+    assert not child_results[1].passed
+    assert child_results[1].status.startswith("publicly accessible")
+
+
 @pytest.mark.vcr
 def test_check_aws_s3_valid():
     """Test CheckAWSS3 check with a valid bucket"""
-    bucket_name = "valid"
+    bucket_name = "testbucket-valid"
 
     # Run the check, which should pass
     check = CheckAWSS3(name="Valid Bucket", value=bucket_name)
