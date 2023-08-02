01ffa347b1485f158f60a618183625dc42ad6fbf
J Lorieau <justin@lorieau.com>
2023-08-02 05:21:25 -0500

    Added import for checkExec

--- geomancy/checks/__init__.py
+++ geomancy/checks/__init__.py
@@ -1,5 +1,6 @@
 from .base import Check, CheckException, Result
 from .env import CheckEnv
+from .exec import CheckExec
 from .path import CheckPath
 from .platform import CheckPlatform
 from .python import CheckPythonPackage
@@ -11,6 +12,7 @@ __all__ = (
     CheckException,
     Result,
     CheckEnv,
+    CheckExec,
     CheckPath,
     CheckPlatform,
     CheckPythonPackage,
