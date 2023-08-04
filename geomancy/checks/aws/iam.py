fa2d51b062264dd261fae81dc82be783b5ff7011
J Lorieau <justin@lorieau.com>
2023-08-04 05:39:36 -0500

    Implement and test CheckAWSIAMAccessKeyAge

--- geomancy/checks/aws/iam.py
+++ geomancy/checks/aws/iam.py
@@ -1,49 +1,96 @@
 """Check AWS credentials, Identity and Access Management (`IAM`_) and security
 best practices.
 
-Security checks include:
+Checks include:
+
+- Authentication with the default or specified profile
+- Access key age (90 days)
 
 
 .. _IAM: https://aws.amazon.com/iam/
 """
 import typing as t
+import logging
+import datetime
 
-from .base import CheckAWS, import_error_msg
+from .base import CheckAWS
 from ..base import Result, Executor
 from ...config import Parameter
+
+logger = logging.getLogger(__name__)
+
+__all__ = ("CheckAWSIAMAccessKeyAge",)
 
 
 class CheckAWSIAMAuthentication(CheckAWS):
     """Checks that the AWS profile can be authenticated"""
 
     msg = Parameter(
-        "CheckAWSIAMAuthentication.MSG",
-        default="Check AWS IAM authentication...",
+        "CHECKAWSIAMAUTHENTICATION.MSG",
+        default="Check AWS IAM authentication",
     )
 
-    import_error_msg = import_error_msg
-
     def check(self, executor: t.Optional[Executor] = None, level: int = 0) -> Result:
-        boto3, exceptions = self.import_modules("boto3", "botocore.exceptions")
+        exceptions = self.import_modules("botocore.exceptions")
 
         # Get the STS client to get details on the
         try:
-            client = self.client("sts")
+            sts = self.client("sts")
         except exceptions.ProfileNotFound:
-            return Result(status="profile not found", msg=self.msg)
+            return Result(status="failed to find profile", msg=self.msg)
 
         # Retrieve caller identity
         try:
-            response = client.get_caller_identity()
+            response = sts.get_caller_identity()
         except exceptions.ClientError:
-            return Result(status="could not authenticate with tokens", msg=self.msg)
+            return Result(status="failed to authenticate with tokens", msg=self.msg)
 
         if "Arn" not in response:
-            return Result(status="could not verify identity with STS", msg=self.msg)
+            return Result(status="failed verify identity with STS", msg=self.msg)
 
         arn = response["Arn"]
         return Result(status=f"passed. ARN='{arn}'")
 
 
-# class CheckAWSIAMRootAccess(CheckAWS):
-#     """Check whether root access credentials exist"""
+class CheckAWSIAMAccessKeyAge(CheckAWS):
+    """Check the age of AWS access keys"""
+
+    #: The key age in days
+    key_age: int
+
+    #: The default key age (in days)
+    key_age_default: int = Parameter("CHECKAWSIAMACCESSKEYAGE.KEY_AGE", default=90)
+
+    msg = Parameter(
+        "CHECKAWSIAMACCESSKEYAGE.MSG",
+        default="Check AWS IAM access key age (check.value days)",
+    )
+
+    def __init__(self, *args, key_age: t.Optional[int] = None, **kwargs):
+        self.key_age = key_age if key_age is not None else self.key_age_default
+        super().__init__(*args, **kwargs)
+
+    def check(self, executor: t.Optional[Executor] = None, level: int = 0) -> Result:
+        msg = self.msg.format(check=self)
+        exceptions = self.import_modules("botocore.exceptions")
+
+        # Get the IAM client
+        try:
+            iam = self.client("iam")
+            response = iam.get_user()
+        except (exceptions.ProfileNotFound, exceptions.ClientError):
+            return Result(status="failed to authenticate host", msg=msg)
+
+        # Get the age of the key
+        if "User" not in response or "CreateDate" not in response["User"]:
+            logger.debug(str(response))
+            return Result(status="failed to get user information", msg=msg)
+
+        # Get the delta in age
+        date = response["User"]["CreateDate"]
+        delta = datetime.datetime.now(tz=None) - date.replace(tzinfo=None)
+
+        if delta.days > self.key_age:
+            return Result(status=f"failed ({delta.days} days)", msg=msg)
+        else:
+            return Result(status="passed", msg=msg)
--- tests/checks/aws/test_iam.py
+++ tests/checks/aws/test_iam.py
@@ -1,8 +1,10 @@
 """Tests for the CheckAWSIAM* classes"""
-import pytest
-import botocore.exceptions
+import datetime
+from unittest.mock import patch
 
-from geomancy.checks.aws.iam import CheckAWSIAMAuthentication
+import pytest
+
+from geomancy.checks.aws.iam import CheckAWSIAMAuthentication, CheckAWSIAMAccessKeyAge
 
 
 @pytest.mark.vcr
@@ -37,3 +39,28 @@ def test_check_aws_iam_authentication():
 
     assert result.passed
     assert result.status.startswith("passed")
+
+
+@pytest.mark.parametrize("age_days", (30, 120))
+@pytest.mark.vcr
+def test_check_aws_aim_access_key_age(age_days):
+    """Test the CheckAWSIAMAccessKeyAge check"""
+    # The following test was run with and the cassette modified to meet
+    # the number of days in 'age_days'
+    check = CheckAWSIAMAccessKeyAge(name="Access key age")
+
+    # Create a mockdatetime to pin a date
+    class mockdatetime(datetime.datetime):
+        @classmethod
+        def now(cls, *args, **kwargs):
+            return datetime.datetime(2023, 8, 4, 5, 31, 11, 895114)
+
+    # Run the check with a pinned date/time
+    with patch("geomancy.checks.aws.iam.datetime.datetime", mockdatetime):
+        result = check.check()
+
+    # Check the age in days
+    if age_days < 90:
+        assert result.passed
+    else:
+        assert not result.passed
